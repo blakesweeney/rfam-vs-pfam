a6d84c967231d692ec9e3c4c94056fa08406ba5e
Blake Sweeney <bsweeney@ebi.ac.uk>
2023-01-30 11:53:09 +0000

    Some updates to plotting
    Should be cleaner now.

--- bin/plot.R
+++ bin/plot.R
@@ -3,9 +3,9 @@
 library(tidyverse)
 
 args = commandArgs(trailingOnly=TRUE)
-structures <- read_csv(args[2])
 
 data <- read_csv(args[1])
+structures <- read_csv(args[2])
 
 light_rfam <- "#F7EFEC"
 medium_rfam <- "#A8887B"
@@ -19,9 +19,9 @@ data$alignment_size <- data$number_of_columns * data$number_seqs
 data$number_of_gaps <- data$alignment_size - data$number_residues
 data$fraction_gap <- data$number_of_gaps / data$alignment_size * 100
 
-pivoted <- data %>% pivot_longer(!rfam_acc & !id & !description & !source & !rna_type, names_to="stat", values_to="value")
-medians <- data %>% 
-    group_by(source) %>% 
+pivoted <- data %>% pivot_longer(!rfam_acc & !description & !source & !rna_type, names_to="stat", values_to="value")
+medians <- data %>%
+    group_by(source) %>%
     summarise(
 	median_cols=median(number_of_columns),
 	median_seqs=median(number_seqs),
@@ -39,8 +39,8 @@ medians <- data %>%
 
 # Rfam families cover a very few types of RNAs
 rna_type_df <- data %>%
-	filter(source == 'Rfam seed') %>%
-	select(id, rna_type, number_seqs) %>%
+	filter(startsWith(source, "Rfam")) %>%
+	select(rfam_acc, rna_type, number_seqs) %>%
 	group_by(rna_type) %>%
 	summarise(
 	  count=n(),
@@ -48,27 +48,24 @@ rna_type_df <- data %>%
 	)
 rna_type_df$fraction = rna_type_df$count / sum(rna_type_df$count)
 
-rna_type_df <- data %>%
-    filter(source == 'Rfam seed') %>%
-    select(id, rna_type, number_seqs) %>%
+rfam_rna_type_df <- data %>%
+    filter(startsWith(source, "Rfam")) %>%
+    select(rfam_acc, rna_type, number_seqs) %>%
     group_by(rna_type) %>%
     summarise(
-	count=n(),
-	"Number of seed sequences"=sum(number_seqs),
+      count=n(),
+      "Number of sequences"=sum(number_seqs),
     ) %>%
     mutate(rna_type=fct_reorder(rna_type, count, .desc=TRUE)) %>%
     rename("Number of families"=count) %>%
     pivot_longer(!rna_type, names_to="stat")
 
-with_structures <- data %>% 
-    left_join(structures, by="rfam_acc") %>% 
-    mutate(number_of_structures=replace_na(number_of_structures, 0))
-
 ## Displayed by counts
-family_counts <- rna_type_df %>%
+family_counts <- rfam_rna_type_df %>%
     filter(stat == "Number of families") %>%
     ggplot(aes(x=reorder(rna_type, -value), y=value)) +
-    geom_bar(stat="identity") + facet_grid(stat ~ ., scales="free_y") +
+    geom_bar(stat="identity") +
+    facet_grid(stat ~ ., scales="free_y") +
     theme(axis.text.x = element_text(angle = 45, vjust=0.9, hjust=1)) +
     labs(x="RNA type", y="Count")
 
@@ -83,8 +80,8 @@ alignment_size <- ggplot(data, aes(x=source, y=number_of_columns, color=source, 
 		  show.legend=FALSE,
 		  inherit.aes=FALSE,
 		  aes(x=source, y=median_cols, label=median_cols, vjust=1.5) ) +
-	scale_color_manual(values = c(medium_pfam, medium_rfam)) +
-	scale_fill_manual(values = c(medium_pfam, medium_rfam)) +
+	scale_color_manual(values = c(light_pfam, medium_pfam, light_rfam, medium_rfam)) +
+	scale_fill_manual(values = c(light_pfam, medium_pfam, light_rfam, medium_rfam)) +
 	labs(y="Number of columns")
 
 ggsave("alignment_size.png", alignment_size, device="png")
@@ -98,8 +95,8 @@ alignment_length <- ggplot(data, aes(x=source, y=number_seqs, color=source, fill
 		  show.legend=FALSE,
 		  inherit.aes=FALSE,
 		  aes(x=source, y=median_seqs, label=median_seqs, hjust=0.5, vjust=-0.5) ) +
-	scale_color_manual(values = c(medium_pfam, medium_rfam)) +
-	scale_fill_manual(values = c(medium_pfam, medium_rfam)) +
+	scale_color_manual(values = c(light_pfam, medium_pfam, light_rfam, medium_rfam)) +
+	scale_fill_manual(values = c(light_pfam, medium_pfam, light_rfam, medium_rfam)) +
 	labs(y="Number of sequences")
 
 ggsave("alignment_length.png", alignment_length, device="png")
@@ -112,8 +109,8 @@ alignment_identity <- ggplot(data, aes(x=source, y=percent_identity, color=sourc
 		  show.legend=FALSE,
 		  inherit.aes=FALSE,
 		  aes(x=source, y=median_identity, label=median_identity, vjust=-0.5) ) +
-	scale_color_manual(values = c(medium_pfam, medium_rfam)) +
-	scale_fill_manual(values = c(medium_pfam, medium_rfam)) +
+	scale_color_manual(values = c(light_pfam, medium_pfam, light_rfam, medium_rfam)) +
+	scale_fill_manual(values = c(light_pfam, medium_pfam, light_rfam, medium_rfam)) +
 	scale_y_continuous(limits=c(0, 100)) +
 	labs(y="Percent identity")
 
@@ -127,8 +124,8 @@ alignment_gaps <- ggplot(data, aes(x=source, y=fraction_gap, color=source, fill=
 		  show.legend=FALSE,
 		  inherit.aes=FALSE,
 		  aes(x=source, y=median_gaps, label=median_gaps, vjust=-0.5) ) +
-	scale_color_manual(values = c(medium_pfam, medium_rfam)) +
-	scale_fill_manual(values = c(medium_pfam, medium_rfam)) +
+	scale_color_manual(values = c(light_pfam, medium_pfam, light_rfam, medium_rfam)) +
+	scale_fill_manual(values = c(light_pfam, medium_pfam, light_rfam, medium_rfam)) +
 	labs(y="Percent of gaps")
 
 ggsave("alignment_gaps.png", alignment_gaps, device="png")
@@ -161,20 +158,23 @@ ggsave("alignment_gaps.png", alignment_gaps, device="png")
 #
 #  grid.arrange(number_residues_p, number_seqs_p, ncol=1)
 
-rna_type_df2 <- with_structures %>%
-  	filter(source == 'Rfam seed') %>%
-  	select(id, rna_type, number_seqs, number_of_structures) %>%
+rfam_structures_df <- data %>%
+    left_join(structures, by="rfam_acc") %>%
+    mutate(rna_type=factor(rna_type)) %>%
+    mutate(number_of_structures=replace_na(number_of_structures, 0)) %>%
+  	filter(startsWith(source, "Rfam")) %>%
+  	select(rfam_acc, rna_type, number_seqs, number_of_structures, source) %>%
   	group_by(rna_type) %>%
   	summarise(
-  	  families=n(),
-  	  seed=sum(number_seqs),
-  	  structures=sum(number_of_structures)) %>% 
-	mutate(rna_type=factor(rna_type)) %>%
-	mutate(rna_type=reorder(rna_type, -families)) %>%
-	pivot_longer(!rna_type, names_to="stat")
+  	  Families=n(),
+  	  "Seed Sequences"=sum(number_seqs[source == "Rfam seed"]),
+      "Full Sequences"=sum(number_seqs[source == "Rfam full"]),
+  	  "Structures"=sum(number_of_structures)) %>%
+    mutate(rna_type=fct_reorder(rna_type, Families, min, .desc=TRUE)) %>%
+    pivot_longer(!rna_type, names_to="stat")
 
- structures_plot <- ggplot(rna_type_df2, aes(x=reorder(rna_type, -value), y=value)) +
+ structures_plot <- ggplot(rfam_structures_df, aes(x=reorder(rna_type, -value), y=value)) +
   	geom_bar(stat="identity") + facet_grid(stat ~ ., scales="free_y") +
-  	theme(axis.text.x = element_text(angle = 45, vjust=0.9, hjust=1)) + labs(x="RNA type", y="Count")
+  	theme(axis.text.x = element_text(angle = 45, vjust=0.9, hjust=1)) + labs(x="Rfam RNA Type", y="Count")
 
 ggsave("rfam_structures.png", structures_plot, device="png")
